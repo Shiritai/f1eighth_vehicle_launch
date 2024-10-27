3e7425534d0e53d59d7d9e4146c9baa8f4e11d51
aeon <jerry73204@gmail.com>
2024-10-28 01:34:22 +0800

    Fix bugs so that vehicle_launch.launch.xml can work

--- f1eighth_vehicle_interface/setup.py
+++ f1eighth_vehicle_interface/setup.py
@@ -1,27 +1,28 @@
 from setuptools import find_packages, setup
+from glob import glob
 
-package_name = 'f1eighth_vehicle_interface'
+package_name = "f1eighth_vehicle_interface"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        (f"share/{package_name}", ["package.xml"]),
+        (f"share/{package_name}/params", glob("params/*")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='habby',
-    maintainer_email='a0979580915@gmail.com',
-    description='Vehicle interface for F1EIGHTH',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer="habby",
+    maintainer_email="a0979580915@gmail.com",
+    description="Vehicle interface for F1EIGHTH",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'actuator = f1eighth_vehicle_interface.actuator:main',
-            'velocity_report = f1eighth_vehicle_interface.velocity_report:main',
+        "console_scripts": [
+            "actuator = f1eighth_vehicle_interface.actuator:main",
+            "velocity_report = f1eighth_vehicle_interface.velocity_report:main",
         ],
     },
 )
